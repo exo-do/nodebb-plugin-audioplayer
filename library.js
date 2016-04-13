33e8983958700658e25aea88059bf9c8536085d6
exomarty <martyalicante@gmail.com>
2016-04-14 00:30:32 +0200

    links code style adjustments

--- library.js
+++ library.js
@@ -6,9 +6,10 @@
     
 	var AudioPlayer = {}, 
 	    type = "",	    	    
-		 embed  = '<div class="playit" data-link="http://$2.$3:$4" data-name="$4" ><p>stream <i class="fa fa-play">&nbsp;&nbsp;</i>$4</p></div>',
+		embed= "",
 		  
-       embedUrl_mpeg = /<a href="(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6}):?([\/\w \.-]*)\/?.*>stream<\/a>/ig,     //regex mp3
+       embedUrl_stream = /<a href="(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6}):?([\/\w \.-]*)\/?.*>stream<\/a>/ig,     //regex stream 
+	   embedUrl_ip = /<a href="(https?:\/\/)?([0-9]|[0-9]{2}|[0-9]{3})\.([0-9]|[0-9]{2}|[0-9]{3})\.([0-9]|[0-9]{2}|[0-9]{3})\.([0-9]|[0-9]{2}|[0-9]{3})(\/|\:)([\/\w \.-]*)\/?.*>stream<\/a>/ig,  //regex ip
        embedUrl_ogg  = /<a href=".*\/uploads\/files\/(\w*-(.*\.ogg)).*>.*<\/a>/ig,   // regex ogg
        embedUrl_wav  = /<a href=".*\/uploads\/files\/(\w*-(.*\.wav)).*>.*<\/a>/ig;   // regex wav
        
@@ -17,10 +18,19 @@
 	    if (!data || !data.postData || !data.postData.content) { 
             return callback(null, data);
         }
-        // mp3
-        if (data.postData.content.match(embedUrl_mpeg)) {
+        // stream
+        if (data.postData.content.match(embedUrl_stream)) {
            type = "audio/mpeg";
-            data.postData.content = data.postData.content.replace(embedUrl_mpeg, embed);
+		    embed  = '<div class="playit" data-link="http://$2.$3:$4" data-name="$4" ><p>stream <i class="fa fa-play">&nbsp;&nbsp;</i>$4</p></div>';
+            data.postData.content = data.postData.content.replace(embedUrl_stream, embed);
+        }
+		
+
+        // stream_ip
+        if (data.postData.content.match(embedUrl_ip)) {
+           type = "audio/mpeg";
+		    embed  = '<div class="playit" data-link="$1$2.$3.$4.$5$6$7" data-name="$7" ><p>stream ip <i class="fa fa-play">&nbsp;&nbsp;</i>$7</p></div>';
+            data.postData.content = data.postData.content.replace(embedUrl_ip, embed);
         }
 
         // ogg
